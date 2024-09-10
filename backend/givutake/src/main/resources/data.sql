b79c9d69db0705a74c0260eca559274e7f18f1f1
김현우 <hwk216@naver.com>
2024-09-10 15:58:35 +0900

    [BE] [fix] sql initializer 구현

--- backend/givutake/src/main/resources/data.sql
+++ backend/givutake/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- Insert initial region data
-INSERT INTO region (region_name, create_date, modified_date)
+INSERT INTO region (region_name, created_date, modified_date)
 VALUES
 ('서울특별시', NOW(), NOW()),
 ('부산광역시', NOW(), NOW()),
@@ -18,3 +18,14 @@ VALUES
 ('제주특별자치도', NOW(), NOW()),
 ('강원특별자치도', NOW(), NOW()),
 ('전북특별자치도', NOW(), NOW());
+
+-- Insert initial category data
+INSERT INTO categories (category_name, category_type, created_date, modified_date)
+values
+    ('지역상품권', 0, now(), now()),
+    ('농축산물', 0, now(), now()),
+    ('수산물', 0, now(), now()),
+    ('가공식품', 0, now(), now()),
+    ('공예품', 0, now(), now()),
+    ('재난재해', 1, now(), now()),
+    ('지역기부', 1, now(), now());
