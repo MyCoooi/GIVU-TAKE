be96522ddf68fe28066ec6b96a1f75913def7a82
강현성 <tjd4025@naver.com>
2024-09-10 12:32:04 +0900

    [FE] [feat] #11 : 답례품 메인페이지 레이아웃

--- frontend/android/app/src/main/java/com/project/givuandtake/MainActivity.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/MainActivity.kt
@@ -10,6 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.project.givuandtake.feature.gift.mainpage.DonationPage
+import com.project.givuandtake.feature.gift.mainpage.DonationPageDetail
 import com.project.givuandtake.ui.theme.GivuAndTakeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GivuAndTakeTheme {
+                val navController = rememberNavController() // NavController 생성
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("givu and take")
+                    // gift 함수를 사용하여 테마 및 공통 스타일 적용
+                    gift {
+                        // 네비게이션 호스트 설정
+                        NavHost(navController = navController, startDestination = "donation_page") {
+                            composable("donation_page") { DonationPage(navController) }
+                            composable("donation_page_detail/{itemIndex}") { backStackEntry ->
+                                val itemIndex = backStackEntry.arguments?.getString("itemIndex")?.toInt() ?: 0
+                                DonationPageDetail(itemIndex)
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +47,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GivuAndTakeTheme {
-        Greeting("Android")
+fun gift(content: @Composable () -> Unit){
+    MaterialTheme {
+        Surface {
+            content()
+        }
     }
 }
