1f1f1050158be8bf0101594270d30b067890ffd6
정유진 <renascor96@naver.com>
2024-09-12 14:13:13 +0900

    [FE] [feat] #23 : 회원가입 2,3 페이지 디자인

--- frontend/android/app/src/main/java/com/project/givuandtake/feature/auth/SingupStep2.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/auth/SingupStep2.kt
@@ -1,19 +1,241 @@
 package com.project.givuandtake.auth
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 
 @Composable
 fun SignupStep2(navController: NavController) {
-    Column {
-        Text("Signup Step 2")
-        // Step 2 관련 UI 요소들 추가
+    var region by remember { mutableStateOf("") }
+    var phoneNumber by remember { mutableStateOf("") }
+    var birthDate by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
 
-        Button(onClick = { navController.navigate("signup_step3") }) {
-            Text("Next")
+    // 전체를 감싸는 외부 박스
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFA726))  // 전체 배경색을 오렌지색으로 설정
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center // 필드들을 세로 중앙으로 배치
+        ) {
+            // 상단 네비게이션과 타이틀 박스
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .background(Color(0xFFFFA726)),
+                contentAlignment = Alignment.Center
+            ) {
+                // 타이틀 텍스트
+                Text(
+                    text = "GIVU & TAKE",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color(0xFFFFFFFF)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // 두 박스를 감싸는 중간 박스
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(500.dp) // 높이를 유지하기 위해 고정
+                    .clip(RoundedCornerShape(30.dp))  // 중간 박스의 모서리 둥글게 처리
+                    .background(Color.White)  // 중간 박스 배경색
+                    .padding(12.dp)  // 중간 박스 패딩
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Top, // 내부 필드들을 위쪽 정렬
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    // o o o 단계를 감싸는 박스
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp) // 중간 박스의 윗부분에 맞춰지도록 패딩 조정
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFE0E0E0))  // 활성화된 단계
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFFFA726))  // 비활성화된 단계
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFE0E0E0))  // 비활성화된 단계
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+                    // 회원가입 텍스트
+                    Text(
+                        text = "회원가입",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp) // 상하 간격을 위한 패딩
+                    )
+
+                    // 지역 선택 입력 필드
+                    OutlinedTextField(
+                        value = region,
+                        onValueChange = { region = it },
+                        label = { Text("지역을 선택해주세요(*필수)") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        trailingIcon = {
+                            Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+                        },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFFFA726),
+                            unfocusedBorderColor = Color.Gray
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    // 전화번호 입력 필드
+                    OutlinedTextField(
+                        value = phoneNumber,
+                        onValueChange = { phoneNumber = it },
+                        label = { Text("전화번호를 입력해주세요(*필수)") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone),
+                        shape = RoundedCornerShape(12.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFFFA726),
+                            unfocusedBorderColor = Color.Gray
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    // 생년월일 입력 필드
+                    OutlinedTextField(
+                        value = birthDate,
+                        onValueChange = { birthDate = it },
+                        label = { Text("생년월일을 입력해주세요(*필수)") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        trailingIcon = {
+                            Icon(Icons.Default.DateRange, contentDescription = null)
+                        },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFFFA726),
+                            unfocusedBorderColor = Color.Gray
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    // 성별 선택 버튼
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Button(
+                            onClick = { gender = "남성" },
+                            colors = if (gender == "남성") ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFFFA726),
+                                contentColor = Color.White
+                            ) else ButtonDefaults.buttonColors(
+                                backgroundColor = Color.White,
+                                contentColor = Color(0xFFFFA726)
+                            ),
+                            border = BorderStroke(1.dp, Color(0xFFFFA726)),
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(8.dp)
+                        ) {
+                            Text("남성", fontWeight = FontWeight.ExtraBold)
+                        }
+
+                        Button(
+                            onClick = { gender = "여성" },
+                            colors = if (gender == "여성") ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFFFA726),
+                                contentColor = Color.White
+                            ) else ButtonDefaults.buttonColors(
+                                backgroundColor = Color.White,
+                                contentColor = Color(0xFFFFA726)
+                            ),
+                            border = BorderStroke(1.dp, Color(0xFFFFA726)),
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(8.dp)
+                        ) {
+                            Text("여성", fontWeight = FontWeight.ExtraBold)
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+
+
+                    Spacer(modifier = Modifier.height(24.dp))  // 입력 필드와 버튼 사이에 여백 추가
+
+                    // 다음 버튼을 입력 필드와 함께 박스 안에 배치
+                    Button(
+                        onClick = { navController.navigate("signup_step3") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .clip(RoundedCornerShape(12.dp)),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFFA726))
+                    ) {
+                        Text("다음", fontSize = 18.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            color = Color.White,
+                            textAlign = TextAlign.Center)
+                    }
+                }
+            }
         }
     }
 }
--- frontend/android/app/src/main/java/com/project/givuandtake/feature/auth/SingupStep3.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/auth/SingupStep3.kt
@@ -1,19 +1,149 @@
-package com.project.givuandtake.feature.auth
+package com.project.givuandtake.auth
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 
 @Composable
 fun SignupStep3(navController: NavController) {
-    Column {
-        Text("Signup Step 3")
-        // Step 3 관련 UI 요소들 추가
 
-        Button(onClick = { navController.navigate("mainpage") }) {
-            Text("Finish")
+    // 전체를 감싸는 외부 박스
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFA726))  // 전체 배경색을 오렌지색으로 설정
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center // 필드들을 세로 중앙으로 배치
+        ) {
+            // 상단 네비게이션과 타이틀 박스
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .background(Color(0xFFFFA726)),
+                contentAlignment = Alignment.Center
+            ) {
+                // 타이틀 텍스트
+                Text(
+                    text = "GIVU & TAKE",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color(0xFFFFFFFF)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // OOO 단계, 건너뛰기, 시작하기 버튼을 감싸는 중간 박스
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(30.dp))  // 둥근 모서리 처리
+                    .background(Color.White)  // 흰색 배경
+                    .padding(16.dp)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    // OOO 단계를 감싸는 박스
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        // ooo 단계
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFE0E0E0))  // 비활성화된 단계 색상
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFE0E0E0))  // 비활성화된 단계 색상
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .clip(CircleShape)
+                                .background(Color(0xFFFFA726))  // 활성화된 단계 색상
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "맞춤설정",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp) // 상하 간격을 위한 패딩
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // 안내 텍스트
+                    Text(
+                        text = "설문에 참여하시면\n맞춤화된 추천을 받으실 수 있어요",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                        textAlign = TextAlign.Center,
+                        color = Color.Black,
+                        modifier = Modifier.fillMaxWidth()  // 텍스트를 박스만큼 크기로 설정
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    // 건너뛰기 버튼
+                    TextButton(
+                        onClick = { navController.navigate("mainpage") }, // 건너뛰기 클릭 시 메인페이지로 이동
+                        modifier = Modifier.align(Alignment.End) // 오른쪽에 배치
+                    ) {
+                        Text(
+                            text = "건너뛰기",
+                            fontSize = 16.sp,
+                            color = Color.Gray,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    // 시작하기 버튼
+                    Button(
+                        onClick = { navController.navigate("mainpage") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .clip(RoundedCornerShape(12.dp)),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFFA726))
+                    ) {
+                        Text("시작하기", fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                    }
+                }
+            }
         }
     }
 }
