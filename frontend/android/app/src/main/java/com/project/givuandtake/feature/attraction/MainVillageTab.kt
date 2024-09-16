ac6c181e1e54c40ca183948ffe897cc369314f0c
진아현 <jah01894@gmail.com>
2024-09-17 01:56:48 +0900

    [FE] [feat] #25 : 관광지 api 요청 도전중

---
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/attraction/MainVillageTab.kt
@@ -0,0 +1,34 @@
+package com.project.givuandtake.feature.attraction
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainVillageTab() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "우리고향 체험마을",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+        Text(
+            text = "전체보기",
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(end=8.dp)
+        )
+    }
+}
