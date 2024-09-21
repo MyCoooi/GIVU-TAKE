7b00a91580b54ba90b985686f8662310d0245686
진아현 <jah01894@gmail.com>
2024-09-22 01:48:12 +0900

    [FE] [feat] #31 : 관광지 세부페이지 api연결 완료 및 디자인 완성

--- frontend/android/app/src/main/java/com/project/givuandtake/feature/attraction/MainVillageTab.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/attraction/MainVillageTab.kt
@@ -1,8 +1,12 @@
 package com.project.givuandtake.feature.attraction
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,21 +18,31 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainVillageTab() {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "우리고향 체험마을",
-            fontSize = 20.sp,
-            modifier = Modifier.padding(start = 8.dp)
-        )
-        Text(
-            text = "전체보기",
-            fontSize = 14.sp,
-            color = Color.Gray,
-            modifier = Modifier.padding(end=8.dp)
-        )
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "우리고향 체험마을",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            Text(
+                text = "전체보기",
+                fontSize = 14.sp,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .clickable {
+                    }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
     }
 }
