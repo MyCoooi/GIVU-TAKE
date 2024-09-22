b6b82622396ab937d35055bd2b618424811409bb
김종훈 <boll0918@naver.com>
2024-09-23 00:21:43 +0900

    [BE] [fix] 실행 환경이 로컬인지 서버인지에 따라 .env 파일의 경로를 다르게 설정하도록 수정

--- recommend/givutake/app/core/config.py
+++ recommend/givutake/app/core/config.py
@@ -1,9 +1,21 @@
 from pydantic import BaseSettings
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+is_ec2 = os.getenv('IS_EC2', 'false').lower() == 'true'
+
+if is_ec2:
+    env_path = Path('/home/ubuntu/build/recommendation/') / '.env'
+else:
+    env_path = Path.cwd().parent / '.env'
+
+load_dotenv(dotenv_path=env_path)
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Location Recommender API"
-    SECRET_KEY: str = "your-secret-key"  # 실제 운영 환경에서는 안전한 값으로 변경해야 합니다
-    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    ALGORITHM: str = os.getenv("JWT_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
