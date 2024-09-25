e1650c7d769293df2db0c61863130818ec946d8e
김종훈 <boll0918@naver.com>
2024-09-25 10:52:41 +0900

    [BE] [fix] FastAPI 서버에서 jwt token을 decode 못하던 문제 해결

--- recommend/givutake/app/core/security.py
+++ recommend/givutake/app/core/security.py
@@ -1,19 +1,22 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2AuthorizationCodeBearer
 import jwt
-from config import settings
+from app.core.config import settings
 import base64
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl="token", tokenUrl="token")
 
-def get_current_user(token: str):
+def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_SECRET_ALGORITHM])
+        print(token)
+        print(settings.JWT_SECRET_ALGORITHM)
+        print(settings.JWT_SECRET_KEY)
+        payload = jwt.decode(token, base64.b64decode(settings.JWT_SECRET_KEY).decode('utf-8'), algorithms=[settings.JWT_SECRET_ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
@@ -25,5 +28,3 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_SECRET_ALGORITHM)
     return encoded_jwt
-
-get_current_user('eyJhbGciOiJIUzUxMiJ9.eyJhdXRoIjoiUk9MRV9DT1JQT1JBVElPTiIsInN1YiI6InNzYWZ5QGV4YW1wbGUuY29tIiwiaXNzIjoiY29tLmFjY2VwdGVkLmdpdnV0YWtlIiwibmJmIjoxNzI3MDU2NzM5LCJpYXQiOjE3MjcwNTY3MzksImV4cCI6MTcyNzA1NzYzOSwianRpIjoiM2YwZjdiMWUtMTlkMS00YTJjLWIzNDItNjA4MDBjYmU0MzA0In0.NgaCeYrDMogqwVNrWneh4zIVkFJxv6Z-1Qf-2wZJG18i12l0MjD8g4I6hOObNAMKhd1wBI3ZmpEA-0EtnaUS1A')
--- recommend/givutake/app/main.py
+++ recommend/givutake/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.api.endpoints import recommendations
 from app.core.config import settings
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(recommendations.router, prefix="/recommend/v1")
+
+is_ec2 = os.getenv('IS_EC2', 'false').lower() == 'true'
+
+if is_ec2:
+    env_path = Path('/home/ubuntu/build/recommendation/') / '.env'
+else:
+    env_path = Path.cwd() / '.env'
+    print(env_path)
+
+load_dotenv(dotenv_path=env_path, override=True)
 
 if __name__ == "__main__":
     import uvicorn
