c27f8014429d83133173d98f5a414c0098a0b67c
김현우 <hwk216@naver.com>
2024-09-27 09:36:20 +0900

    [BE] [feat] 기부 총액 api 생성

--- backend/givutake/src/main/java/com/accepted/givutake/gift/repository/OrderRepository.java
+++ backend/givutake/src/main/java/com/accepted/givutake/gift/repository/OrderRepository.java
@@ -6,9 +6,11 @@ import com.accepted.givutake.user.common.entity.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
     Page<Orders> findByUsers(Users user, Pageable pageable);
     int countByGift(Gifts gift);
-
+    @Query("SELECT SUM(o.price) FROM Orders o")
+    Integer getTotalOrderPrice();
 }
