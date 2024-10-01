3fbfaade4e5ea0b060fa514406eec90624da3839
진아현 <jah01894@gmail.com>
2024-10-02 01:20:39 +0900

    [FE] [feat] #39 : 주소 삭제 완성, 주소등록 완성, 주소 수정 api 회의 필요

--- frontend/android/app/src/main/java/com/project/givuandtake/feature/mypage/MyActivities/AddressBook.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/mypage/MyActivities/AddressBook.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -44,11 +45,82 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.project.givuandtake.R
 import com.project.givuandtake.core.apis.Address.AddressApi
+import com.project.givuandtake.core.apis.Address.AddressDeleteApi
 import com.project.givuandtake.core.data.Address.AddressData
 import com.project.givuandtake.core.data.Address.UserAddress
 import com.project.givuandtake.core.datastore.TokenManager
 import kotlinx.coroutines.launch
 import retrofit2.Response
+
+@Composable
+fun AddressItem(address: UserAddress) {
+    Column() {
+        Row(
+            modifier = Modifier
+                .padding(start = 15.dp, end = 15.dp, top = 8.dp, bottom = 0.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (address.addressName == "우리집") {
+                Icon(
+                    painter = painterResource(id = R.drawable.house),
+                    contentDescription = "Icon",
+                    tint = Color(0xFFA093DE),
+                    modifier = Modifier.size(24.dp)
+                )
+            } else if (address.addressName == "회사" ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.company),
+                    contentDescription = "Icon",
+                    tint = Color(0xFFA093DE),
+                    modifier = Modifier.size(24.dp)
+                )
+            } else {
+                Icon(
+                    painter = painterResource(id = R.drawable.human),
+                    contentDescription = "Icon",
+                    tint = Color(0xFFA093DE),
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Column() {
+                Row() {
+                    Text(text = address.addressName, fontSize = 16.sp)
+                    if (address.representative) {
+                        Box(
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp)
+                                .background(
+                                    Color(0xFFFF6F6F),
+                                    shape = RoundedCornerShape(50)
+                                ) // 빨간색 배경과 둥근 모서리
+                                .padding(horizontal = 20.dp, vertical = 2.dp)
+                        ) {
+                            Text(
+                                text = "대표 주소지",
+                                color = Color.White,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                }
+
+                Text(text = "${address.roadAddress} ${address.detailAddress}", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(text = address.jibunAddress, fontSize = 13.sp, color = Color.Gray)
+            }
+        }
+        Divider(
+            color = Color(0xFFF2F2F2),
+            thickness = 3.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        )
+    }
+}
+
 
 class AddressViewModel : ViewModel() {
 
@@ -79,7 +151,6 @@ class AddressViewModel : ViewModel() {
 fun AddressBook(navController: NavController) {
     val context = LocalContext.current
     val accessToken = "Bearer ${TokenManager.getAccessToken(context)}"
-    Log.d("asdfasdfasdfasdf", "$accessToken")
 
     val viewModel: AddressViewModel = viewModel()
 
@@ -104,19 +175,24 @@ fun AddressBook(navController: NavController) {
                 modifier = Modifier
                     .size(28.dp)
                     .clickable { navController.popBackStack() }
-                    .weight(0.3f)
             )
 
-            Spacer(modifier = Modifier.weight(0.7f))
+            Spacer(modifier = Modifier.weight(1f))
 
             Text(
                 text = "주소록",
                 textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .weight(1f), fontSize = 20.sp
+                fontSize = 20.sp
             )
 
             Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = "편집",
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                modifier = Modifier.clickable{ navController.navigate("addressbookupdate")}
+            )
         }
 
         Box(
@@ -205,73 +281,6 @@ fun AddressBook(navController: NavController) {
         } else {
             Text(text = "등록된 주소가 없습니다.", modifier = Modifier.padding(16.dp))
         }
-    }
-}
-
-@Composable
-fun AddressItem(address: UserAddress) {
-    Column() {
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 15.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            if (address.addressName == "우리집") {
-                Icon(
-                    painter = painterResource(id = R.drawable.house),
-                    contentDescription = "Icon",
-                    tint = Color(0xFFA093DE),
-                    modifier = Modifier.size(24.dp)
-                )
-            } else if (address.addressName == "회사" ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.company),
-                    contentDescription = "Icon",
-                    tint = Color(0xFFA093DE),
-                    modifier = Modifier.size(24.dp)
-                )
-            } else {
-                Icon(
-                    painter = painterResource(id = R.drawable.human),
-                    contentDescription = "Icon",
-                    tint = Color(0xFFA093DE),
-                    modifier = Modifier.size(24.dp)
-                )
-            }
-            Spacer(modifier = Modifier.width(15.dp))
-            Column() {
-                Row() {
-                    Text(text = address.addressName, fontSize = 16.sp)
-                    if (address.representative) {
-                        Box(
-                            modifier = Modifier
-                                .padding(horizontal = 10.dp)
-                                .background(Color(0xFFFF6F6F), shape = RoundedCornerShape(50)) // 빨간색 배경과 둥근 모서리
-                                .padding(horizontal = 20.dp, vertical = 2.dp)
-                        ) {
-                            Text(
-                                text = "대표 주소지",
-                                color = Color.White,
-                                fontSize = 12.sp
-                            )
-                        }
-                    }
-                }
-//                Spacer(modifier = Modifier.height(2.dp))
-                Text(text = "${address.roadAddress} ${address.detailAddress}", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(text = address.jibunAddress, fontSize = 13.sp, color = Color.Gray)
-            }
-        }
-        Divider(
-            color = Color(0xFFF2F2F2),
-            thickness = 3.dp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
     }
 }
 
