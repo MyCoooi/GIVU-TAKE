3a3a8cf95423d1cd5daffef529dcc5530deeb948
Yujin Jeong <universe_adhoc@naver.com>
2024-10-01 12:00:02 +0900

    [FE] [feat] #59: 펀딩 상세페이지 댓글 전체 조회

--- frontend/android/app/src/main/java/com/project/givuandtake/core/apis/Funding/FundingCommentsApi.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/core/apis/Funding/FundingCommentsApi.kt
@@ -1,19 +1,18 @@
 package com.project.givuandtake.core.apis.Funding
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-// 댓글 응답 데이터 클래스
+// 댓글 조회에 대한 응답 데이터 클래스
 data class CommentResponse(
     val success: Boolean,
     val data: List<CommentData>
 )
 
+// 댓글 데이터 클래스
 data class CommentData(
     val commentIdx: Int,
     val name: String,
@@ -22,31 +21,22 @@ data class CommentData(
 )
 
 // Retrofit API 정의
-interface FundingDetailCommentApiService {
+interface FundingCommentsService {
     @GET("government-fundings/{fundingIdx}/comments")
-    fun getComments(
+    fun getFundingComments(
         @Path("fundingIdx") fundingIdx: Int
     ): Call<CommentResponse>
 }
 
 // Retrofit 인스턴스 생성
-object FundingDetailCommentApi {
+object FundingCommentsApi {
     private const val BASE_URL = "https://j11e202.p.ssafy.io/api/"
 
-    val api: FundingDetailCommentApiService by lazy {
-        // HttpLoggingInterceptor 추가
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .build()
-
+    val api: FundingCommentsService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)  // OkHttpClient 설정
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(FundingDetailCommentApiService::class.java)
+            .create(FundingCommentsService::class.java)
     }
 }
