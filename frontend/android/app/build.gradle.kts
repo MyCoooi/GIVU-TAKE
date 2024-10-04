17f4b5da4d372b7f3d3871ae156eac610d877c7a
강현성 <tjd4025@naver.com>
2024-10-04 14:20:09 +0900

    [FE][!hotfix] 기부 종료 긴급수정

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     // Room 추가
     implementation("androidx.room:room-runtime:2.6.1")
-//    kapt("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
     implementation("com.naver.maps:map-sdk:3.19.1")
