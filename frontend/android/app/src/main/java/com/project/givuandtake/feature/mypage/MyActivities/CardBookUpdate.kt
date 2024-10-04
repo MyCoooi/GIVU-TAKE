d7a7671c16b63f3778c40bd38d6307140c703d45
진아현 <jah01894@gmail.com>
2024-10-04 17:10:13 +0900

    [FE] [feat] #39 : 카드 완료

--- frontend/android/app/src/main/java/com/project/givuandtake/feature/mypage/MyActivities/CardBookUpdate.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/mypage/MyActivities/CardBookUpdate.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -51,11 +52,14 @@ import com.project.givuandtake.core.apis.Address.AddressApi
 import com.project.givuandtake.core.apis.Address.AddressDeleteApi
 import com.project.givuandtake.core.apis.Address.AddressUpdateApi
 import com.project.givuandtake.core.apis.Card.CardApi
+import com.project.givuandtake.core.apis.Card.CardDeleteApi
+import com.project.givuandtake.core.apis.Card.CardUpdateApi
 import com.project.givuandtake.core.data.Address.AddressData
 import com.project.givuandtake.core.data.Address.AddressPostData
 import com.project.givuandtake.core.data.Address.AddressUpdateData
 import com.project.givuandtake.core.data.Address.UserAddress
 import com.project.givuandtake.core.data.Card.CardData
+import com.project.givuandtake.core.data.Card.CardUpdateData
 import com.project.givuandtake.core.data.Card.UserCard
 import com.project.givuandtake.core.datastore.TokenManager
 import kotlinx.coroutines.launch
@@ -97,42 +101,43 @@ class CardUpdateViewModel : ViewModel() {
         }
     }
 
-//    fun updateAddress(token: String, addressIdx: Int, addressData: AddressUpdateData, onUpdateSuccess: () -> Unit, onError: () -> Unit) {
-//        viewModelScope.launch {
-//            try {
-//                val response = AddressUpdateApi.api.updateAddressData(token, addressIdx, addressData)
-//                if (response.isSuccessful) {
-//                    Log.d("AddressUpdate", "주소 수정 성공")
-//                    onUpdateSuccess()
-//                } else {
-//                    Log.e("AddressUpdate", "주소 수정 실패: ${response.code()}")
-//                    onError()
-//                }
-//            } catch (e: Exception) {
-//                Log.e("AddressUpdate", "예외 발생: ${e.message}", e)
-//                onError()
-//            }
-//        }
-//    }
-//    fun deleteAddress(token: String, addressIdx: Int, onDeleteSuccess: () -> Unit, onError: () -> Unit) {
-//        viewModelScope.launch {
-//            try {
-//                val response = AddressDeleteApi.api.deleteAddress(token = "$token", addressIdx = addressIdx)
-//                if (response.isSuccessful) {
-//                    Log.d("AddressDelete", "주소 삭제 성공")
-//                    // 삭제된 아이템을 리스트에서 제거
-//                    _addresses.value = _addresses.value.filter { it.addressIdx != addressIdx }
-//                    onDeleteSuccess()
-//                } else {
-//                    Log.e("AddressDelete", "주소 삭제 실패: ${response.code()}")
-//                    onError()
-//                }
-//            } catch (e: Exception) {
-//                Log.e("AddressDelete", "예외 발생: ${e.message}", e)
-//                onError()
-//            }
-//        }
-//    }
+    fun deleteCard(token: String, cardIdx: Int, onDeleteSuccess: () -> Unit, onError: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                val response = CardDeleteApi.api.deleteCard(token = "$token", CardIdx = cardIdx)
+                if (response.isSuccessful) {
+                    Log.d("CardDelete", "카드 삭제 성공")
+                    // 삭제된 아이템을 리스트에서 제거
+                    _cards.value = _cards.value.filter { it.cardIdx != cardIdx }
+                    onDeleteSuccess()
+                } else {
+                    Log.e("CardDelete", "카드 삭제 실패: ${response.code()}")
+                    onError()
+                }
+            } catch (e: Exception) {
+                Log.e("CardDelete", "예외 발생: ${e.message}", e)
+                onError()
+            }
+        }
+    }
+
+    fun updateCard(token: String, cardIdx: Int, cardData: CardUpdateData, onUpdateSuccess: () -> Unit, onError: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                val response = CardUpdateApi.api.updateCardData(token, cardIdx, cardData)
+                if (response.isSuccessful) {
+                    Log.d("CardUpdate", "카드 수정 성공")
+                    onUpdateSuccess()
+                } else {
+                    Log.e("CardUpdate", "카드 수정 실패: ${response.code()}")
+                    onError()
+                }
+            } catch (e: Exception) {
+                Log.e("CardUpdate", "예외 발생: ${e.message}", e)
+                onError()
+            }
+        }
+    }
 }
 
 
@@ -156,24 +161,13 @@ val bankUpdateList = listOf(
 )
 
 @Composable
-fun CardUpdateList(cards: List<UserCard>, bankList: List<CardBank>) {
-    LazyColumn {
-        items(cards) { card ->
-            val bank = bankList.firstOrNull { it.name == card.cardCompany } // 은행 찾기
-            CardUpdateItem(card, bank)
-        }
-    }
-}
-
-@Composable
-fun CardUpdateItem(card: UserCard, bank: CardBank?) {
+fun CardUpdateItem(card: UserCard, bank: CardBank?, viewModel: CardUpdateViewModel, accessToken: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        // 은행 로고
         if (bank != null) {
             Image(
                 painter = painterResource(id = bank.imageRes),
@@ -186,129 +180,129 @@ fun CardUpdateItem(card: UserCard, bank: CardBank?) {
             )
         }
 
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(10.dp))
 
         Column (
-            modifier = Modifier.weight(3f)
+            modifier = Modifier.weight(5f)
         ){
-            // 은행 이름
-            Text(
-                text = card.cardCompany,
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            )
+            Row() {
+                Text(
+                    text = card.cardCompany,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                if( card.representative ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .background(
+                                Color(0xFFFF6F6F),
+                                shape = RoundedCornerShape(50)
+                            ) // 빨간색 배경과 둥근 모서리
+                            .padding(horizontal = 20.dp, vertical = 2.dp)
+                    ) {
+                        Text(
+                            text = "대표 카드",
+                            color = Color.White,
+                            fontSize = 12.sp
+                        )
+                    }
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .weight(1.6f)
+                    ) {
+                        Text(
+                            text = "",
+                            color = Color.White,
+                            fontSize = 12.sp
+                        )
+                    }
+                }
 
-            // 카드 번호 (마스킹 처리)
+            }
             Text(
                 text = maskCardUpdateNumber(card.cardNumber),
-                fontSize = 14.sp
+                fontSize = 15.sp
             )
         }
-        if( card.representative ) {
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 10.dp)
-                    .background(
-                        Color(0xFFFF6F6F),
-                        shape = RoundedCornerShape(50)
-                    ) // 빨간색 배경과 둥근 모서리
-                    .padding(horizontal = 20.dp, vertical = 2.dp)
-                    .weight(1.6f)
-            ) {
-                Text(
-                    text = "대표 카드",
-                    color = Color.White,
-                    fontSize = 12.sp
-                )
-            }
-        } else {
-            Box(
-                modifier = Modifier
-                    .weight(1.6f)
-            ) {
-                Text(
-                    text = "",
-                    color = Color.White,
-                    fontSize = 12.sp
-                )
-            }
-        }
+
     }
     Row(
-        modifier = Modifier.fillMaxWidth().padding(10.dp),
-        horizontalArrangement = Arrangement.End
     ) {
-        if ( !card.representative ) {
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 10.dp)
-                    .background(
-                        Color.White,
-                        shape = RoundedCornerShape(50)
-                    ) // 빨간색 배경과 둥근 모서리
-                    .padding(horizontal = 20.dp, vertical = 2.dp)
-                    .weight(1.6f)
-                    .border(BorderStroke(1.dp, Color(0xFFA093DE))),
-            ) {
-                Text(
-                    text = "삭제",
-                    color = Color.Red,
-                    fontSize = 12.sp
-                )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            if ( !card.representative ) {
+                OutlinedButton(
+                    onClick = {
+                        val cardData = CardUpdateData(
+                            isRepresentative = true
+                        )
+                        viewModel.updateCard(
+                            token = accessToken,
+                            cardIdx = card.cardIdx,
+                            cardData = cardData,
+                            onUpdateSuccess = {
+                                Log.d("AddressUpdateItem", "수정 성공")
+                                viewModel.getCardData(accessToken)
+                            },
+                            onError = {
+                                Log.e("AddressUpdateItem", "수정 실패")
+                            }
+                        )
+                    },
+                    shape = RoundedCornerShape(15.dp),
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .height(28.dp),
+                    border = BorderStroke(1.dp, Color(0xFFA093DE)),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xFFA093DE)),
+                    contentPadding = PaddingValues(0.dp)
+                ) {
+                    Text(
+                        text = "대표 주소지로 설정",
+                        modifier = Modifier.padding(vertical = 0.dp, horizontal = 20.dp),
+                        fontSize = 10.sp
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
             }
             OutlinedButton(
                 onClick = {
-//                    val addressData = AddressUpdateData(
-//                        isRepresentative = true
-//                    )
-//                    viewModel.updateAddress(
-//                        token = accessToken,
-//                        addressIdx = address.addressIdx,
-//                        addressData = addressData,
-//                        onUpdateSuccess = {
-//                            Log.d("AddressUpdateItem", "수정 성공")
-//                            viewModel.fetchUserAddresses(accessToken)
-//                        },
-//                        onError = {
-//                            Log.e("AddressUpdateItem", "수정 실패")
-//                        }
-//                    )
+                    viewModel.deleteCard(
+                        token = accessToken,
+                        cardIdx = card.cardIdx,
+                        onDeleteSuccess = {
+                            Log.d("AddressUpdateItem", "삭제 성공")
+                        },
+                        onError = {
+                            Log.e("AddressUpdateItem", "삭제 실패")
+                        }
+                    )
                 },
                 shape = RoundedCornerShape(15.dp),
-                modifier = Modifier.padding(vertical = 0.dp),
-                border = BorderStroke(1.dp, Color(0xFFA093DE)),
-                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xFFA093DE))
+                modifier = Modifier
+                    .padding(0.dp)
+                    .height(28.dp),
+                border = BorderStroke(1.dp, Color(0xFFFF6F6F)),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xFFFF6F6F)),
+                contentPadding = PaddingValues(0.dp)
             ) {
                 Text(
-                    text = "대표 주소지로 설정",
-                    modifier = Modifier.padding(vertical = 0.dp, horizontal = 10.dp).padding(0.dp)
+                    text = "삭제",
+                    modifier = Modifier.padding(vertical = 0.dp, horizontal = 0.dp),
+                    fontSize = 10.sp
                 )
             }
-            Spacer(modifier = Modifier.width(8.dp))
-        }
-        OutlinedButton(
-            onClick = {
-//                viewModel.deleteAddress(
-//                    token = accessToken,
-//                    addressIdx = address.addressIdx,
-//                    onDeleteSuccess = {
-//                        Log.d("AddressUpdateItem", "삭제 성공")
-//                    },
-//                    onError = {
-//                        Log.e("AddressUpdateItem", "삭제 실패")
-//                    }
-//                )
-            },
-            shape = RoundedCornerShape(15.dp),
-            border = BorderStroke(1.dp, Color(0xFFFF6F6F)),
-            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xFFFF6F6F))
-        ) {
-            Text(
-                text = "삭제",
-            )
-        }
 
+        }
     }
+    Divider()
 }
 
 fun maskCardUpdateNumber(cardNumber: String): String {
@@ -359,26 +353,16 @@ fun CardBookUpdate(navController: NavController) {
         }
 
         if (cards.isNotEmpty()) {
-            CardUpdateList(cards = cards, bankList = bankList)
+            LazyColumn {
+                items(cards) { card ->
+                    val bank = bankList.firstOrNull { it.name == card.cardCompany } // 은행 찾기
+                    CardUpdateItem(card, bank, viewModel, accessToken)
+                }
+            }
         } else {
             Text(text = "", modifier = Modifier.padding(16.dp))
         }
-
-
-//        if (addresses.isNotEmpty()) {
-//            LazyColumn(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//            ) {
-//                items(addresses) { address ->
-//                    AddressUpdateItem(address = address, viewModel = viewModel, accessToken = accessToken)
-//                }
-//            }
-//        } else {
-//            Text(text = "등록된 주소가 없습니다.", modifier = Modifier.padding(16.dp))
-//        }
     }
-
 }
 
 //@Composable
