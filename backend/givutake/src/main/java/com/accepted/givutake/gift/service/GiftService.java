c31d23f179aa37221b384dc5642f2afe8dff9f39
김현우 <hwk216@naver.com>
2024-10-07 11:33:18 +0900

    [BE] [fix] 답례품 s3 설정

--- backend/givutake/src/main/java/com/accepted/givutake/gift/service/GiftService.java
+++ backend/givutake/src/main/java/com/accepted/givutake/gift/service/GiftService.java
@@ -7,13 +7,13 @@ import com.accepted.givutake.gift.model.*;
 import com.accepted.givutake.gift.repository.GiftRepository;
 import com.accepted.givutake.gift.repository.GiftReviewLikedRepository;
 import com.accepted.givutake.gift.repository.GiftReviewRepository;
+import com.accepted.givutake.global.service.S3Service;
 import com.accepted.givutake.payment.entity.Orders;
 import com.accepted.givutake.payment.repository.OrderRepository;
 import com.accepted.givutake.global.entity.Categories;
 import com.accepted.givutake.global.enumType.ExceptionEnum;
 import com.accepted.givutake.global.exception.ApiException;
 import com.accepted.givutake.global.repository.CategoryRepository;
-import com.accepted.givutake.payment.service.OrderService;
 import com.accepted.givutake.user.common.entity.Users;
 import com.accepted.givutake.user.common.model.UserDto;
 import com.accepted.givutake.user.common.repository.UsersRepository;
@@ -24,7 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
@@ -44,17 +46,38 @@ public class GiftService {
     private final UsersRepository userRepository;
     private final UserService userService;
     private final OrderRepository orderRepository;
-    private final OrderService orderService;
+    private final S3Service s3Service;
 
-    public Gifts createGift(String email, CreateGiftDto request) {
+    public Gifts createGift(String email, CreateGiftDto request, MultipartFile thumbnailImage, MultipartFile contentImage) {
+        System.out.println(request.getCategoryIdx());
         Categories category = categoryRepository.findById(request.getCategoryIdx()).orElseThrow(() -> new ApiException(ExceptionEnum.NOT_FOUND_CATEGORY_EXCEPTION));
         UserDto savedUserDto = userService.getUserByEmail(email);
         Users corporation = savedUserDto.toEntity();
+        String thumbnailImageUrl = null;
+        String contentImageUrl = null;
+
+        if(!thumbnailImage.isEmpty()){
+            try{
+                thumbnailImageUrl = s3Service.uploadProfileImage(thumbnailImage);
+            } catch(IOException e){
+                throw new ApiException(ExceptionEnum.ILLEGAL_GIFT_THUMBNAIL_IMAGE_EXCEPTION);
+            }
+        }
+
+        if(!contentImage.isEmpty()){
+            try{
+                contentImageUrl = s3Service.uploadContentImage(contentImage);
+            } catch(IOException e){
+                throw new ApiException(ExceptionEnum.ILLEGAL_GIFT_CONTENT_IMAGE_EXCEPTION);
+            }
+        }
+
         Gifts newGift = Gifts.builder()
                 .giftName(request.getGiftName())
                 .corporations(corporation)
                 .category(category)
-                .giftThumbnail(request.getGiftThumbnail())
+                .giftThumbnail(thumbnailImageUrl)
+                .giftContentImage(contentImageUrl)
                 .giftContent(request.getGiftContent())
                 .price(request.getPrice())
                 .build();
@@ -106,6 +129,7 @@ public class GiftService {
                 .giftIdx(gift.getGiftIdx())
                 .giftName(gift.getGiftName())
                 .giftThumbnail(gift.getGiftThumbnail())
+                .giftContentImage(gift.getGiftContent())
                 .giftContent(gift.getGiftContent())
                 .corporationIdx(gift.getCorporations().getUserIdx())
                 .corporationName(gift.getCorporations().getName())
@@ -119,13 +143,39 @@ public class GiftService {
                 .build();
     }
 
-    public Gifts updateGift(String email, int giftIdx, UpdateGiftDto request) {
+    public Gifts updateGift(String email, int giftIdx, UpdateGiftDto request, MultipartFile thumbnailImage, MultipartFile contentImage) {
         Gifts gift = giftRepository.findById(giftIdx).orElseThrow(() -> new ApiException(ExceptionEnum.NOT_FOUND_GIFT_EXCEPTION));
         if(!gift.getCorporations().getEmail().equals(email)){
             throw new ApiException(ExceptionEnum.ACCESS_DENIED_EXCEPTION);
         }
+        String thumbnailImageUrl = null;
+        String contentImageUrl = null;
+
+        if(!thumbnailImage.isEmpty()){
+            try{
+                if(gift.getGiftThumbnail()!=null)s3Service.deleteThumbnailImage(gift.getGiftThumbnail());
+                thumbnailImageUrl = s3Service.uploadProfileImage(thumbnailImage);
+            } catch(IOException e){
+                throw new ApiException(ExceptionEnum.ILLEGAL_GIFT_THUMBNAIL_IMAGE_EXCEPTION);
+            }
+        }else{
+            thumbnailImageUrl = gift.getGiftThumbnail();
+        }
+
+        if(!contentImage.isEmpty()){
+            try{
+                if(gift.getGiftContentImage()!=null)s3Service.deleteContentImage(gift.getGiftContentImage());
+                contentImageUrl = s3Service.uploadContentImage(contentImage);
+            } catch(IOException e){
+                throw new ApiException(ExceptionEnum.ILLEGAL_GIFT_CONTENT_IMAGE_EXCEPTION);
+            }
+        }else{
+            contentImageUrl = gift.getGiftContentImage();
+        }
+
         gift.setGiftName(request.getGiftName());
-        gift.setGiftThumbnail(request.getGiftThumbnail());
+        gift.setGiftThumbnail(thumbnailImageUrl);
+        gift.setGiftContentImage(contentImageUrl);
         gift.setGiftContent(request.getGiftContent());
         gift.setCategory(categoryRepository.findById(request.getCartegoryIdx()).orElseThrow(() -> new ApiException(ExceptionEnum.NOT_FOUND_CATEGORY_EXCEPTION)));
         gift.setPrice(request.getPrice());
