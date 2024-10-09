060eab087f6220c1e1691503c3927dc4ffdfd585
강현성 <tjd4025@naver.com>
2024-10-10 08:52:06 +0900

    [FE][feat] #75 : 장바구니 api 연결 완료

--- frontend/android/app/src/main/java/com/project/givuandtake/feature/gift/CartPage.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/gift/CartPage.kt
@@ -1,6 +1,8 @@
 package com.project.givuandtake.feature.gift
 
 import android.content.Context
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,19 +16,22 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.project.givuandtake.R
 import com.project.givuandtake.core.data.CartItemData
 import com.project.givuandtake.core.datastore.TokenManager
 import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Composable
-fun CartPage(navController: NavController, context: Context) {
+fun CartPage(navController: NavController, context: Context, cartViewModel: CartViewModel = viewModel()) {
     val coroutineScope = rememberCoroutineScope()
-    var cartItems by remember { mutableStateOf(emptyList<CartItemData>()) }  // 장바구니 항목 상태
+    var cartItems by remember { mutableStateOf(emptyList<CartItemData>()) }
     val scaffoldState = rememberScaffoldState()
-
+//    val cartItems by cartViewModel.cartItems.collectAsState() // ViewModel의 상태를 UI에서 수집
     // 장바구니 데이터를 API에서 불러오는 함수
     LaunchedEffect(Unit) {
         coroutineScope.launch {
@@ -39,13 +44,14 @@ fun CartPage(navController: NavController, context: Context) {
                             cartIdx = cartItemData.cartIdx,
                             giftIdx = cartItemData.giftIdx,
                             giftName = cartItemData.giftName,
-                            giftThumbnail = cartItemData.giftThumbnail,  // 추가된 필드
-                            userIdx = cartItemData.userIdx,              // 추가된 필드
+                            giftThumbnail = cartItemData.giftThumbnail,
+                            userIdx = cartItemData.userIdx,
                             price = cartItemData.price,
                             amount = cartItemData.amount,
                             location = cartItemData.location
                         )
                     }
+                    cartViewModel.setCartItems(cartItems) // ViewModel에 데이터 설정
                 } else {
                     scaffoldState.snackbarHostState.showSnackbar("Failed to load cart items")
                 }
@@ -55,7 +61,7 @@ fun CartPage(navController: NavController, context: Context) {
 
     Scaffold(
         topBar = { CartTopBar() },
-        bottomBar = { CartBottomBar(totalAmount = cartItems.sumOf { it.price * it.amount }) },
+        bottomBar = { CartBottomBar(navController, cartItems) },  // 결제 버튼에서 데이터 넘기기 추가
         scaffoldState = scaffoldState
     ) { innerPadding ->
         LazyColumn(
@@ -76,6 +82,7 @@ fun CartPage(navController: NavController, context: Context) {
                                     if (it == item) it.copy(amount = newQuantity) else it
                                 }
                                 cartItems = updatedItems
+                                cartViewModel.setCartItems(updatedItems) // ViewModel에 UI 갱신
                             }
                         }
                     },
@@ -85,16 +92,21 @@ fun CartPage(navController: NavController, context: Context) {
                             if (result) {
                                 // 삭제 성공 시 UI 업데이트
                                 cartItems = cartItems.filter { it != item }
+                                cartViewModel.setCartItems(cartItems) // ViewModel에 데이터 갱신
+                                val updatedItems = cartItems.filter { it != item }
+                                cartViewModel.setCartItems(updatedItems) // ViewModel에 데이터 갱신
+
                             }
                         }
                     },
-                    context = context  // context 전달
+                    context = context
                 )
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
 }
+
 
 @Composable
 fun CartTopBar() {
@@ -105,21 +117,33 @@ fun CartTopBar() {
 }
 
 @Composable
-fun CartBottomBar(totalAmount: Int) {
+fun CartBottomBar(navController: NavController, cartItems: List<CartItemData>) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        // 결제 총 금액
+        // 결제 총 금액 계산
+        val totalAmount = cartItems.sumOf { it.price * it.amount }
+
+        // 결제 총 금액 표시
         Text(text = "결제 총 금액", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        Text(text = "₩$totalAmount", fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color(0xFF4099E9))
+        Text(text = "₩${String.format("%,d", totalAmount)}", fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color(0xFF4099E9))
 
         Spacer(modifier = Modifier.height(8.dp))
 
         // 결제하기 버튼
         Button(
-            onClick = { /* TODO: 결제 페이지로 이동 */ },
+            onClick = {
+                if (cartItems.isNotEmpty()) {
+                    // 첫 번째 cartItem을 URL 매개변수로 전달
+                    val firstItem = cartItems[0]
+                    val name = "${firstItem.giftName}..포함 ${cartItems.size}개"
+                    navController.navigate(
+                        "payment_page_gift?name=${name}&location=${firstItem.location}&price=${totalAmount}&quantity=${firstItem.amount}&thumbnailUrl=${firstItem.giftThumbnail}&giftIdx=${firstItem.giftIdx}"
+                    )
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp),
@@ -155,16 +179,29 @@ fun CartItemView(
                 Column {
                     Text(text = cartItem.giftName, fontSize = 18.sp, fontWeight = FontWeight.Bold)
                     Text(text = "수량: ${cartItem.amount}", fontSize = 14.sp)
-                    Text(text = cartItem.location ?: "위치 정보 없음", fontSize = 14.sp, color = Color.Gray)
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically, // 아이콘과 텍스트를 세로로 중앙 정렬
+                        horizontalArrangement = Arrangement.Start // 아이템 간 간격을 설정 (필요에 따라 조정 가능)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_location_on_24), // 이미지 리소스를 불러옴
+                            contentDescription = "원산지",
+                            modifier = Modifier.size(16.dp), // 아이콘 크기 설정
+                            tint = Color.Gray // 아이콘 색상 설정
+                        )
+                        Spacer(modifier = Modifier.width(2.dp)) // 아이콘과 텍스트 사이 간격
+                        Text(
+                            text = cartItem.location ?: "위치 정보 없음", // location이 null이면 기본 텍스트 표시
+                            fontSize = 14.sp, // 텍스트 크기 설정
+                            color = Color.Gray // 텍스트 색상 설정
+                        )
+                    }
                 }
                 Spacer(modifier = Modifier.weight(1f))
 
                 IconButton(onClick = {
                     coroutineScope.launch {
-//                        val result = deleteCartItem(context, cartItem.cartIdx)
-//                        if (result) {
                         onDeleteItem(cartItem)
-//                        }
                     }
                 }) {
                     Icon(
@@ -214,7 +251,7 @@ fun CartItemView(
                 }
                 Spacer(modifier = Modifier.weight(1f))
 
-                Text(text = "₩${cartItem.price * cartItem.amount}", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                Text(text = "₩${String.format("%,d", cartItem.price * cartItem.amount)}", fontSize = 18.sp, fontWeight = FontWeight.Bold)
             }
         }
     }
--- frontend/android/app/src/main/java/com/project/givuandtake/feature/payment/Payment_gift.kt
+++ frontend/android/app/src/main/java/com/project/givuandtake/feature/payment/Payment_gift.kt
@@ -255,6 +255,7 @@
 //
 package com.project.payment
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -266,6 +267,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -275,6 +278,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -292,10 +296,168 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.project.givuandtake.PaymentsActivity
+import com.project.givuandtake.core.data.CartItemData
 import com.project.givuandtake.core.data.KakaoPaymentInfo
+import com.project.givuandtake.feature.gift.CartViewModel
 import com.project.givuandtake.feature.payment.PaymentMethods_gift
 import com.project.givuandtake.feature.payment.PaymentViewModel
 import com.project.givuandtake.feature.payment.TossPaymentScreen
+
+//@Composable
+//fun PaymentScreen_gift(
+//    navController: NavController,
+//    cartItems: List<CartItemData>, // 여러 상품의 리스트를 받아옵니다.
+//    viewModel: PaymentViewModel = viewModel(),
+//    cartViewModel: CartViewModel = viewModel()
+//) {
+//    var selectedMethod by remember { mutableStateOf("KAKAO") } // 결제 수단 상태
+//    val context = LocalContext.current
+//    val cartItems by cartViewModel.cartItems.collectAsState()
+//    Log.d("cartItems","cartItems 뷰 확인: ${cartItems}")
+//    Surface(
+//        modifier = Modifier.fillMaxSize(),
+//        color = Color.White
+//    ) {
+//        Column(
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            // 여러 상품 정보를 표시하는 컴포저블
+//            LazyColumn(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(16.dp)
+//            ) {
+//                items(cartItems) { cartItem ->
+//                    PaymentProjectInfo_gift(
+//                        name = cartItem.giftName,
+//                        location = cartItem.location ?: "위치 정보 없음",
+//                        quantity = cartItem.amount,
+//                        thumbnailUrl = cartItem.giftThumbnail ?: ""
+//                    )
+//                }
+//            }
+//
+//            Spacer(modifier = Modifier.height(16.dp))
+//
+//            // 결제 수단 선택 UI
+//            PaymentMethods_gift(
+//                selectedMethod = selectedMethod,
+//                onMethodSelected = { method -> selectedMethod = method }
+//            )
+//
+//            Spacer(modifier = Modifier.height(24.dp))
+//
+//            // 결제 총 금액 및 버튼 UI
+//            val totalAmount = cartItems.sumOf { it.amount * it.price }
+//            PaymentTotalAndButton2_gift(
+//                cartItems = cartItems,
+//                paymentMethod = selectedMethod,
+//                navController = navController,
+//                viewModel = viewModel,
+//                totalAmount = totalAmount
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun PaymentTotalAndButton2_gift(
+//    cartItems: List<CartItemData>,
+//    paymentMethod: String,
+//    navController: NavController,
+//    viewModel: PaymentViewModel,
+//    totalAmount: Int // 총 금액을 받아옴
+//) {
+//    Surface(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(16.dp),
+//        color = Color.White,
+//        shadowElevation = 4.dp
+//    ) {
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(16.dp),
+//            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            // 결제 총 금액 표시
+//            PaymentTotal_gift(totalAmount)
+//
+//            // 결제하기 버튼
+//            PaymentButton_gift(
+//                cartItems = cartItems,
+//                paymentMethod = paymentMethod,
+//                navController = navController,
+//                viewModel = viewModel,
+//                totalAmount = totalAmount // 총 결제 금액 전달
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun PaymentButton_gift(
+//    cartItems: List<CartItemData>,
+//    paymentMethod: String,
+//    navController: NavController,
+//    viewModel: PaymentViewModel,
+//    totalAmount: Int // 총 결제 금액을 전달
+//) {
+//    val context = LocalContext.current
+//
+//    Button(
+//        onClick = {
+//            // 여러 상품을 한 번에 결제 처리
+//            val kakaoPaymentInfo = KakaoPaymentInfo(
+//                paymentMethod = paymentMethod,
+//                amount = totalAmount, // 총 결제 금액 전달
+//                giftIdx = cartItems[0].giftIdx // 여러 상품일 경우 첫 번째 상품 ID 전달
+//            )
+//
+//            viewModel.preparePayment(
+//                navController = navController,
+//                context = context,
+//                paymentInfo = kakaoPaymentInfo
+//            )
+//        },
+//        shape = RoundedCornerShape(24.dp),
+//        modifier = Modifier
+//            .height(50.dp)
+//            .width(150.dp),
+//        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF9C88FF)) // 배경색 설정
+//    ) {
+//        Text(
+//            text = "결제하기",
+//            fontSize = 16.sp,
+//            fontWeight = FontWeight.Bold,
+//            color = Color.White
+//        )
+//    }
+//}
+//
+//@Composable
+//fun PaymentTotal_gift(totalAmount: Int) {
+//    Column(
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        modifier = Modifier
+//            .padding(end = 16.dp)
+//    ) {
+//        Text(
+//            text = "결제 총 금액",
+//            fontSize = 14.sp,
+//            color = Color(0xFF1E88E5)
+//        )
+//        Spacer(modifier = Modifier.height(4.dp))
+//        Text(
+//            text = "₩${String.format("%,d", totalAmount)}",
+//            fontSize = 18.sp,
+//            fontWeight = FontWeight.Bold,
+//            color = Color.Black
+//        )
+//    }
+//}
 
 @Composable
 fun PaymentScreen_gift(
